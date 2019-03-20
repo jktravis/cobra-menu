d646179909cc2bf6d26691542d1283ded6f0f60b
jktravis <joshua.travis@gmail.com>
2019-03-19 23:54:20 -0400

    Add envelope icon

--- src/QuickToolList.js
+++ src/QuickToolList.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { FaHouseDamage, FaMap, FaQuestionCircle } from "react-icons/fa";
+import {
+  FaEnvelope,
+  FaHouseDamage,
+  FaMap,
+  FaQuestionCircle,
+} from "react-icons/fa";
 import styled from "react-emotion";
 
 const QuickToolListStyle = styled("div")`
   display: flex;
-  width: 5rem;
-  margin-top: .25rem;
+  width: 7rem;
+  margin-top: 0.25rem;
   justify-content: space-between;
   font-size: 1.2rem;
   color: ${({ theme }) => theme.darkGray};
@@ -21,13 +26,16 @@ function QuickToolList() {
   return (
     <QuickToolListStyle>
       <a href="#">
-        <FaHouseDamage/>
+        <FaHouseDamage />
       </a>
       <a href="#">
-        <FaMap/>
+        <FaMap />
       </a>
       <a href="#">
-        <FaQuestionCircle/>
+        <FaQuestionCircle />
+      </a>
+      <a href="#">
+        <FaEnvelope />
       </a>
     </QuickToolListStyle>
   );

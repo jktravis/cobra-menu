947ec710b96308f4830d15428b40738148ec6fb4
jktravis <joshua.travis@gmail.com>
2019-03-20 07:21:09 -0400

    Updates

---
+++ precache-manifest.fe49e49c0fdb20e457c6984d8c9096ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d214fe6d2fa72a0d8068",
+    "url": "/cobra-menu/static/js/runtime~main.d417c8ef.js"
+  },
+  {
+    "revision": "edc873527c4c82d4176d",
+    "url": "/cobra-menu/static/js/main.bc657ec5.chunk.js"
+  },
+  {
+    "revision": "d9ac66e93bd0e18f12da",
+    "url": "/cobra-menu/static/js/2.1c5b9225.chunk.js"
+  },
+  {
+    "revision": "edc873527c4c82d4176d",
+    "url": "/cobra-menu/static/css/main.4bf5386c.chunk.css"
+  },
+  {
+    "revision": "a78889701956d149d888c598246b1b3e",
+    "url": "/cobra-menu/index.html"
+  }
+];

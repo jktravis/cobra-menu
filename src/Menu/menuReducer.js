bf4d7cdd545252742ed81cebb56e1351d96c92c7
jktravis <joshua.travis@gmail.com>
2019-03-19 21:54:54 -0400

    Refactored reducer

--- src/Menu/menuReducer.js
+++ src/Menu/menuReducer.js
@@ -1,42 +1,45 @@
 import * as R from "ramda";
 
 const initialState = {
-  expandedMenus: []
+  openSubMenus: [],
+  expandMenu: false
 };
 
 const MenuActions = Object.freeze({
-  TOGGLE_MENU: "TOGGLE_MENU"
+  TOGGLE_SUB_MENU: "TOGGLE_SUB_MENU",
+  EXPAND_MENU: "EXPAND_MENU"
 });
 
-const expandedLens = R.lensProp('expandedMenus');
+const openSubMenuLens = R.lensProp("openSubMenus");
 
-const toggleItem = R.curry((id, data) => R.ifElse(R.includes(id), R.without(id), R.append(id))(data));
+const toggleSubMenu = R.curry((id, data) =>
+  R.ifElse(R.includes(id), R.without(id), R.append(id))(data)
+);
 
 const reducer = (state, action) => {
   const { type, payload } = action;
   switch (type) {
-    case MenuActions.TOGGLE_MENU:
-      return R.over(
-        expandedLens,
-        toggleItem(payload.id),
-        state
-      );
+    case MenuActions.TOGGLE_SUB_MENU:
+      return R.over(openSubMenuLens, toggleSubMenu(payload.id), state);
 
     default:
       return state;
   }
 };
 
-function toggleMenuAction(id) {
+function toggleSubMenuAction(id) {
   return {
-    type: MenuActions.TOGGLE_MENU,
+    type: MenuActions.TOGGLE_SUB_MENU,
     payload: { id }
   };
 }
 
 function bindActionCreators(dispatch) {
   return {
-    toggleMenu: R.compose(dispatch, toggleMenuAction)
+    toggleSubMenu: R.compose(
+      dispatch,
+      toggleSubMenuAction
+    )
   };
 }
 
